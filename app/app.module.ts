bf08c790ff585bed4d0ba633f87ff1e757c3663d
bad4iz <bad4iz@gmail.com>
2017-01-22 13:23:57 +0400

    module

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,8 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
  * Created by bad4iz on 21.01.2017.
  */
 
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser'
+import {AppComponent} from './app.component';
+
 @NgModule({
     imports: [BrowserModule],
     declarations: [AppComponent],
