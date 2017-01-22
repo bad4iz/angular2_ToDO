c3d97a7baf745eb78ba6f939a66e73783d3a7b1f
bad4iz <bad4iz@gmail.com>
2017-01-22 13:40:57 +0400

    html css

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,9 +2,12 @@ import {Component} from '@angular/core';
 
 
 @Component({
+    moduleId: module.id,
     selector: 'app',
-    template: '<h1>Angular 2Do </h1>'
+    templateUrl: './app.component.html',
+    styleUrls: ['app.component.css']
 })
 export class AppComponent{
+    title = 'Angular 2Do'
 
 }
