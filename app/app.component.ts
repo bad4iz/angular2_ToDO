ad1f8679e17fecb04ed7f62ff160c64f060ac80a
bad4iz <bad4iz@gmail.com>
2017-01-24 21:58:54 +0400

    x

--- app/app.component.ts
+++ app/app.component.ts
@@ -11,17 +11,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
     title = 'Angular 2Do';
-    todos:Todo[] = todos;
-
-    toggle(todo: Todo){
-        todo.completed = !todo.completed;
-    }
-    delete(todo: Todo){
-        let index = this.todos.indexOf(todo);
-
-        if (index > -1){
-            this.todos.splice(index, 1);
-        }
-    }
 
 }
--- app/todo-list/todo-list.component.ts
+++ app/todo-list/todo-list.component.ts
@@ -11,4 +11,15 @@ import {todos} from '../shared/data';
 })
 export class TodoListComponent{
     todos: Todo[] = todos;
+
+     toggle(todo: Todo){
+        todo.completed = !todo.completed;
+    }
+    delete(todo: Todo){
+        let index = this.todos.indexOf(todo);
+
+        if (index > -1){
+            this.todos.splice(index, 1);
+        }
+    }
 }
