f6981d655f0c8eb87362b4a73bace7753f841fc8
bad4iz <bad4iz@gmail.com>
2017-01-24 22:09:03 +0400

    item

---
+++ app/todo-item/todo-item.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'todo-item',
+    templateUrl: 'todo-item.component.html',
+    styleUrls: ['todo-item.component.css']
+})
+export class TodoItemComponent{
+    
+}
